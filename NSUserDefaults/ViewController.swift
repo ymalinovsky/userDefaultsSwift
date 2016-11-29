4fc5434ebcfcd1a33b3c7ef823c33e4027cb50ee
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-11-29 17:21:56 +0200

    Initial Commit

---
+++ NSUserDefaults/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSUserDefaults
+//
+//  Created by Yan Malinovsky on 29.11.16.
+//  Copyright © 2016 Yan Malinovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
