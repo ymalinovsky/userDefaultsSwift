e907a852d289498110a881f105724f65ce6362f5
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-11-29 18:07:37 +0200

    Setting up NSUserDefaultsClass and Storyboard

--- NSUserDefaults/ViewController.swift
+++ NSUserDefaults/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var playerLabel: UILabel!
+    
+    @IBOutlet var scoreLabel: UILabel!
+    
+    @IBOutlet var hightScore: UILabel!
+    
+    @IBOutlet var leaderName: UILabel!
+    
+    @IBOutlet var userName: UITextField!
+    
+    let userDefaults = UserDefaults.standard
+    
+    var users: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addPlayerAction(_ sender: UIButton) {
+        let currentPlayer: String = userName.text!
+        playerLabel.text = currentPlayer
+        users.append(currentPlayer)
+        
+        userDefaults.set(users, forKey: "usernames")
+    }
+
+    @IBAction func addToScoreAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func resetScoreAndPlayerAction(_ sender: UIButton) {
+        
+    }
 
 }
 
